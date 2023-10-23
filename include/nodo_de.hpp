64e4065d5b84d0cc42c0ffbdac1254d4d28e0c7a
denualper <128816490+denualper@users.noreply.github.com>
2023-10-23 10:19:55 -0300

    Update nodo_de.hpp

--- include/nodo_de.hpp
+++ include/nodo_de.hpp
@@ -9,33 +9,56 @@ private:
     Nodo_de<T>* siguiente;
 public:
     // Constructor.
-    Nodo_de(T dato);
+    Nodo_de(T dato){
+        dato{} = dato;
+        anterior = nullptr;
+        siguiente = nullptr;
+    };
 
     // Constructor.
-    Nodo_de(T dato, Nodo_de<T>* anterior, Nodo_de<T>* siguiente);
+    Nodo_de(T dato, Nodo_de<T>* anterior, Nodo_de<T>* siguiente){
+        dato{} = dato;
+        anterior = anterior;
+        siguiente = siguiente;
+    };
 
     // Pre: -
     // Post: Devuelve un puntero al nodo anterior.
-    Nodo_de<T>* obtener_anterior();
+    Nodo_de<T>* obtener_anterior(){
+        return anterior;
+    };
 
     // Pre: -
     // Post: Devuelve un puntero al nodo siguiente.
-    Nodo_de<T>* obtener_siguiente();
+    Nodo_de<T>* obtener_siguiente(){
+        return siguiente;
+    };
 
     // Pre: -
     // Post: Cambia el puntero al nodo anterior.
-    void cambiar_anterior(Nodo_de<T>* nuevo);
+    void cambiar_anterior(Nodo_de<T>* nuevo){
+        anterior = nuevo;
+    };
 
     // Pre: -
     // Post: Cambia el puntero al nodo siguiente.
-    void cambiar_siguiente(Nodo_de<T>* nuevo);
+    void cambiar_siguiente(Nodo_de<T>* nuevo){
+        siguiente = nuevo;
+    };
 
     // Pre: -
     // Post: Devuelve el dato almacenado.
-    T obtener_dato();
+    T obtener_dato(){
+        return dato{};
+    };
 
     // Destructor.
-    ~Nodo_de();
+    ~Nodo_de(){
+        delete anterior;
+        anterior = nullptr;
+        delete siguiente;
+        siguiente = nullptr;
+    };
 };
 
 #endif
