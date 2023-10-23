66cd3afd2a42b5910f7bbd701bedd2bc3d124647
denualper <128816490+denualper@users.noreply.github.com>
2023-10-23 10:20:31 -0300

    Update cola.hpp

--- include/cola.hpp
+++ include/cola.hpp
@@ -22,31 +22,73 @@ private:
     size_t cantidad_datos;
 public:
     // Constructor.
-    Cola();
+    Cola(){
+        cantidad_datos = 0;
+        primer_nodo = nullptr;
+        ultimo_nodo = nullptr;
+    };
 
     // Pre: -
     // Post: Agrega el dato al final de la cola.
-    void alta(T dato);
+    void alta(T dato){
+        if(cantidad_datos == 0){
+            Nodo<T>* nuevo_nodo = new Nodo(dato);
+            primer_nodo = nuevo_nodo;
+            ultimo_nodo = nuevo_nodo;
+        } else {
+        Nodo<T>* nuevo_nodo = new Nodo(dato);
+        ultimo_nodo->siguiente = nuevo_nodo;
+        ultimo_nodo = nuevo_nodo;
+        cantidad_datos++;
+        }
+    };
 
     // Pre: La cola no puede estar vacía.
     // Post: Elimina el primer dato.
-    T baja();
+    T baja(){
+        bool cola_esta_vacia = vacio();
+        if (cola_esta_vacia){
+            throw Cola_exception();
+        }
+        T dato_a_devolver = primer_nodo->dato;
+        cantidad_datos--;
+        Nodo<T>* aux = primer_nodo->siguiente;
+        delete primer_nodo;
+        primer_nodo = aux;
+        return dato_a_devolver;
+    };
 
     // Pre: La cola no puede estar vacía.
     // Post: Devuelve el primer dato en la cola.
-    T primero();
+    T primero(){
+        bool cola_esta_vacia = vacio();
+        if (cola_esta_vacia){
+            throw Cola_exception();
+        }
+        return primer_nodo->dato;
+    };
 
     // Pre: La cola no puede estar vacía.
     // Post: Devuelve el último dato en la cola.
-    T ultimo();
+    T ultimo(){
+        bool cola_esta_vacia = vacio();
+        if (cola_esta_vacia){
+            throw Cola_exception();
+        }
+        return ultimo_nodo->dato;
+    };
 
     // Pre: -
     // Post: Devuelve la cantidad de datos de la cola.
-    size_t tamanio();
+    size_t tamanio(){
+        return cantidad_datos
+    };
 
     // Pre: -
     // Post: Devuelve true si la cola esta vacía (es decir, si no hay datos).
-    bool vacio();
+    bool vacio(){
+        return (cantidad_datos == 0);
+    };
 
     // El constructor de copia está deshabilitado.
     Cola(const Cola& l) = delete;
@@ -55,7 +97,17 @@ public:
     void operator=(const Cola& l) = delete;
 
     // Destructor.
-    ~Cola();
+    ~Cola(){
+        Nodo<T>* actual = primer_nodo;
+        Nodo<T>* aux;
+        for(int i = 0; i < cantidad_datos; i++){
+            aux = actual->siguiente;
+            delete actual;
+            actual = aux;
+        }
+        actual = nullptr;
+        aux = nullptr;
+     };
 };
 
 #endif
